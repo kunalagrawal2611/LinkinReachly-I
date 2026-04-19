d165b54c86df3fa0a679088c8137cde53b3ef540
Kunal Agrawal <kunalagarwal2611@gmail.com>
2026-04-18 21:33:52 -0400

    fix: education degree level misclassifies MS/MA/MPH as Bachelors

--- src/core/easy-apply-field-map.ts
+++ src/core/easy-apply-field-map.ts
@@ -190,10 +190,10 @@ export function buildEasyApplyProfileFieldMap(profile: ApplicantProfile): Record
   const edu = profile.background.educationSummary || ''
   if (edu.trim()) {
     const hasMBA = /mba/i.test(edu)
-    const hasMasters = hasMBA || /master|mph|ms\b|m\.s\./i.test(edu)
+    const hasMasters = hasMBA || /master|mph|bms\b|m\.s\./i.test(edu)
     const hasBachelors = hasMasters || /bachelor|bsc|b\.s\.|b\.a\./i.test(edu)
     const hasHighSchool = hasBachelors // if you have a bachelor's, you completed HS
-    const degreeLevel = hasMBA ? "Master's Degree" : hasBachelors ? "Bachelor's Degree" : "High School"
+    const degreeLevel = hasMasters ? "Master's Degree" : hasBachelors ? "Bachelor's Degree" : "High School"
     profileFieldMap['What is the highest level of education you have completed'] = degreeLevel
     profileFieldMap['Highest level of education'] = degreeLevel
     profileFieldMap['highest degree'] = degreeLevel
